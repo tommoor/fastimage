93193e619b484630af6776390f77cc39836200c7
Nicolas Grekas <nicolas.grekas@gmail.com>
2014-03-18 08:55:16 +0100

    fix #1

--- Fastimage.php
+++ Fastimage.php
@@ -211,8 +211,7 @@ class FastImage
 		$result = substr($this->str, $this->strpos, $n);
 		$this->strpos += $n;
 		
-		// we are dealing with bytes here, so force the encoding
-		return mb_convert_encoding($result, "8BIT");
+		return $result;
 	}
 
 
